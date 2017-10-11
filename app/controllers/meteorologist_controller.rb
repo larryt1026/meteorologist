1d6edbef8f86461ff477df837ec435a0587abc74
Larry <larryt10@chicagobooth.edu>
2017-10-11 16:20:33 +0000

    meteorologist app

--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -15,17 +15,55 @@ class MeteorologistController < ApplicationController
     # The street address that the user typed is in the variable @street_address.
     # ==========================================================================
 
+    @a ="AIzaSyDwRwOHVpUPaooZCqs-MgWiEVcXFzjyQLw"
+    @key = "433c9d8855eb539384b8fd3f59abcf9e"
 
+    def geocode_url
+       conc_addy = @street_address.gsub("+", "")
+       url = "https://maps.googleapis.com/maps/api/geocode/json?address=
+       #{conc_addy}&key=#{@a}"
+       parsed_data = JSON.parse(open(url).read)
+      return parsed_data
+    end
+  
+    def lat
+      latitude = geocode_url["results"][0]["geometry"]["location"]["lat"]
+      return latitude
+    end
+    
+    def long 
+      longitude = geocode_url["results"][0]["geometry"]["location"]["lng"]
+      return longitude
+    end
+    
+    def forecast_url
+      url = "https://api.darksky.net/forecast/#{@key}/#{lat},#{long}"
+      parsed_data_forecast = JSON.parse(open(url).read)
+      return parsed_data_forecast
+    end
+    
+    current = forecast_url.dig("currently")
+     
+    @current_temperature = current["temperature"]
 
-    @current_temperature = "Replace this string with your answer."
+    @current_summary = current["summary"]
 
-    @current_summary = "Replace this string with your answer."
+    #============================next hour======================================
 
-    @summary_of_next_sixty_minutes = "Replace this string with your answer."
+    hour = forecast_url.dig("minutely")
+    
+    @summary_of_next_sixty_minutes = hour["summary"]
+    
+    #============================sev hours======================================
 
-    @summary_of_next_several_hours = "Replace this string with your answer."
+    sev_hours = forecast_url.dig("hourly")
+    
+    @summary_of_next_several_hours = sev_hours["summary"]
+  
+    #============================sev days=======================================
 
-    @summary_of_next_several_days = "Replace this string with your answer."
+    sev_days = forecast_url.dig("daily")
+    @summary_of_next_several_days = sev_days["summary"]
 
     render("meteorologist/street_to_weather.html.erb")
   end
